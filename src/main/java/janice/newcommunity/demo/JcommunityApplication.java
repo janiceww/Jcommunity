032600760f2d2689a71537f35f84471d9793884c
janiceww <janicewang.127@gmail.com>
2019-08-25 23:35:39 -0400

    init repo

---
+++ src/main/java/janice/newcommunity/demo/JcommunityApplication.java
@@ -0,0 +1,13 @@
+package janice.newcommunity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcommunityApplication.class, args);
+    }
+
+}
